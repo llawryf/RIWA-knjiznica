1e77e68e8aa10b847699e17c495fda37c52789db
llawryf <lcelcner@veleri.hr>
2024-11-07 12:26:23 +0100

    zad 2

--- frontend/quasar-project/src/pages/PopisKnjigaPage.vue
+++ frontend/quasar-project/src/pages/PopisKnjigaPage.vue
@@ -5,43 +5,60 @@
     <p>ovdje možete naći popis dostupnih knjiga</p>
 
     <table border="1" cellpadding="10" cellspacing="0">
-<thead>
-<tr>
-<th>ID</th>
-<th>Naslov</th>
-<th>Autor</th>
-<th>Opis</th>
-<th>Slika</th>
-<th>Stanje</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>1</td>
-<td>GOTH</td>
-<td>Otsuichi</td>
-<td>Japanese Murder Mystery</td>
-<td><img src="" alt="GOTH"></td>
-<td>3</td>
-</tr>
-<tr>
-<td>2</td>
-<td>Pinokijeve avanture</td>
-<td>Carlo Collodi</td>
-<td>Drveni decko postaje pravi</td>
-<td><img src="" alt="Knjiga 2"></td>
-<td>6</td>
-</tr>
-<tr>
-<td>3</td>
-<td>Bijeli klaun</td>
-<td>Damir Miloš</td>
-<td>Mali klaun uci o emocijama</td>
-<td><img src="https://www.knjigolov.hr/slike/slika649.png" alt="Knjiga 3"></td>
-<td>8</td>
-</tr>
-</tbody>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Naslov</th>
+      <th>Autor</th>
+      <th>Opis</th>
+      <th>Slika</th>
+      <th>Stanje</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>6</td>
+      <td>Pride and Prejudice</td>
+      <td>Jane Austen</td>
+      <td>Romantična priča o ljubavi i društvenim normama.</td>
+      <td><img src="" alt="Pride and Prejudice"></td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>The Catcher in the Rye</td>
+      <td>J.D. Salinger</td>
+      <td>Priča o pubertetskoj potrazi za smislom života.</td>
+      <td><img src="" alt="The Catcher in the Rye"></td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>The Odyssey</td>
+      <td>Homer</td>
+      <td>Ep o Odisejevom povratku kući nakon Trojanskog rata.</td>
+      <td><img src="" alt="The Odyssey"></td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Brave New World</td>
+      <td>Aldous Huxley</td>
+      <td>Dystopijska vizija futurističkog društva.</td>
+      <td><img src="" alt="Brave New World"></td>
+      <td>6</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>Crime and Punishment</td>
+      <td>Fyodor Dostoevsky</td>
+      <td>Psihološki roman o kriminalu i kajanju.</td>
+      <td><img src="" alt="Crime and Punishment"></td>
+      <td>7</td>
+    </tr>
+  </tbody>
 </table>
+
   </q-page>
 </template>
 
